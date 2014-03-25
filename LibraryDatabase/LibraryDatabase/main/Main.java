cfd49190016fd7771fec469f4069c2a4f70cc11a
ScottMastro <scott-mastro@hotmail.com>
2014-03-24 22:23:33 -0700

    Main GUI form complete

--- LibraryDatabase/LibraryDatabase/main/Main.java
+++ LibraryDatabase/LibraryDatabase/main/Main.java
@@ -44,30 +44,33 @@ public class Main extends JFrame implements ActionListener{
 		BorderLayout layout = new BorderLayout();
 
 		app.setLayout(layout);
-		
-		app.add(generalInterface(), BorderLayout.SOUTH);
+
 		app.add(clerkInterface(), BorderLayout.WEST);
 		app.add(borrowerInterface(), BorderLayout.CENTER);
+		app.add(librarianInterface(), BorderLayout.EAST);
+		app.add(generalInterface(), BorderLayout.SOUTH);
 
 		
+
+
 		app.setSize(700, 600);
 		app.repaint();
 	}
 
 
-	private static final String CHECK_OUT_NAME = "Check Out Items";
 	private static final String ADD_BORROWER_NAME = "Add Borrower";
+	private static final String CHECK_OUT_NAME = "Check Out Items";
 	private static final String RETURN_ITEM_NAME = "Return Item";
 	private static final String CHECK_OVERDUE_NAME = "Check Overdue Items";
 
 
 	private static JPanel clerkInterface(){
 		JPanel panel = new JPanel();
-		
+
 		TitledBorder title;
 		title = BorderFactory.createTitledBorder("Clerk Options");
 		panel.setBorder(title);
-		
+
 		BoxLayout layout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
 
 		panel.setLayout(layout);
@@ -83,23 +86,26 @@ public class Main extends JFrame implements ActionListener{
 		Button returnItem = new Button(RETURN_ITEM_NAME);
 		returnItem.addActionListener(app);
 		panel.add(returnItem);
-		
+
 		Button checkOverdue = new Button(CHECK_OVERDUE_NAME);
 		checkOverdue.addActionListener(app);
 		panel.add(checkOverdue);
 
 		return panel;
 	}
-	
+
 	private static final String SEARCH_NAME = "Search";
-	
+	private static final String CHECK_ACCOUNT_NAME = "Check Account";
+	private static final String HOLD_REQUEST_NAME = "Make Hold Request";
+	private static final String PAY_FINE_NAME = "Pay Fine";
+
 	private static JPanel borrowerInterface(){
 		JPanel panel = new JPanel();
-		
+
 		TitledBorder title;
 		title = BorderFactory.createTitledBorder("Borrower Options");
 		panel.setBorder(title);
-		
+
 		BoxLayout layout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
 
 		panel.setLayout(layout);
@@ -108,28 +114,70 @@ public class Main extends JFrame implements ActionListener{
 		search.addActionListener(app);
 		panel.add(search);
 
+		Button checkAccount = new Button(CHECK_ACCOUNT_NAME);
+		checkAccount.addActionListener(app);
+		panel.add(checkAccount);
+
+		Button holdRequest = new Button(HOLD_REQUEST_NAME);
+		holdRequest.addActionListener(app);
+		panel.add(holdRequest);
+
+		Button payFine = new Button(PAY_FINE_NAME);
+		payFine.addActionListener(app);
+		panel.add(payFine);
 
 		return panel;
 	}
 	
+	private static final String ADD_BOOK_NAME = "Add Book";
+	private static final String CHECKOUT_REPORT_NAME = "Checkout Report";
+	private static final String POPULAR_BOOK_NAME = "Popular Book Report";
+ 
+
+	private static JPanel librarianInterface(){
+		JPanel panel = new JPanel();
+
+		TitledBorder title;
+		title = BorderFactory.createTitledBorder("Librarian Options");
+		panel.setBorder(title);
+
+		BoxLayout layout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
+
+		panel.setLayout(layout);
+
+		Button addBook = new Button(ADD_BOOK_NAME);
+		addBook.addActionListener(app);
+		panel.add(addBook);
+		
+		Button checkoutReport = new Button(CHECKOUT_REPORT_NAME);
+		checkoutReport.addActionListener(app);
+		panel.add(checkoutReport);
+		
+		Button popularBooks = new Button(POPULAR_BOOK_NAME);
+		popularBooks.addActionListener(app);
+		panel.add(popularBooks);
+
+		return panel;
+	}
+
 	private static JPanel generalInterface(){
 		JPanel panel = new JPanel();
-		
+
 		TitledBorder title;
 		title = BorderFactory.createTitledBorder("Output");
 		panel.setBorder(title);
-		
+
 		JTextArea box = new JTextArea();
 		JScrollPane scroll = new JScrollPane(box);
-		
+
 		box.setSize(100, 300);
 		box.setColumns(60);
 		box.setRows(16);
 		box.setAutoscrolls(true);
 		box.setEditable(false);
-				
+
 		panel.add(scroll);
-		
+
 		return panel;		
 	}
 
@@ -146,14 +194,32 @@ public class Main extends JFrame implements ActionListener{
 
 		if(e.getActionCommand() == RETURN_ITEM_NAME)
 			returnItem();
-		
+
 		if(e.getActionCommand() == CHECK_OVERDUE_NAME)
 			checkOverdue();
-		
+
 		if(e.getActionCommand() == SEARCH_NAME)
 			search();
-	}
 
+		if(e.getActionCommand() == CHECK_ACCOUNT_NAME)
+			checkAccount();
+
+		if(e.getActionCommand() == HOLD_REQUEST_NAME)
+			holdRequest();
+
+		if(e.getActionCommand() == PAY_FINE_NAME)
+			payFine();
+		
+		if(e.getActionCommand() == ADD_BOOK_NAME)
+			addBook();
+		
+		if(e.getActionCommand() == CHECKOUT_REPORT_NAME)
+			checkoutReport();
+		
+		if(e.getActionCommand() == POPULAR_BOOK_NAME)
+			popularBooks();
+		
+	}
 
 	/* 
 	 * Add a new borrower to the library. The user should provide all the required information
@@ -183,8 +249,8 @@ public class Main extends JFrame implements ActionListener{
 		sinOrStNo = properties[6];
 		expiryDate = properties[7];
 		type = properties[8];
-		
-		
+
+
 		JOptionPane.showMessageDialog(null, "ERROR MESSAGE", "Error", JOptionPane.ERROR_MESSAGE);
 
 
@@ -289,7 +355,7 @@ public class Main extends JFrame implements ActionListener{
 		// TODO: update all tuples
 
 	}
-	
+
 	/*
 	 * Checks overdue items. The system displays a list of the items that are overdue and
 	 * the borrowers who have checked them out. The clerk may decide to send an email messages
@@ -297,14 +363,70 @@ public class Main extends JFrame implements ActionListener{
 	 */
 	private void checkOverdue() {
 		// TODO everything
-		
+
 	}
-	
+
 	/*
 	 * Search for books using keyword search on titles, authors and subjects. The result is a 
 	 * list of books that match the search together with the number of copies that are in and out.
 	 */
 	private void search() {
+		// TODO everything
+
+	}
+
+	/*
+	 * Check his/her account. The system will display the items the borrower has currently borrowed and
+	 * not yet returned, any outstanding fines and the hold requests that have been placed by the borrower.
+	 */
+	private void checkAccount() {
+		// TODO everything
+
+	}
+
+	/*
+	 * Place a hold request for a book that is out. When the item is returned, the system sends an email
+	 * to the borrower and informs the library clerk to keep the book out of the shelves.
+	 */
+	private void holdRequest() {
+		// TODO everything
+
+	}
+
+	/*
+	 * Pay a fine.
+	 */
+	private void payFine() {
+		// TODO Everything
+
+	}
+	
+	/*
+	 * Adds a new book or new copy of an existing book to the library. The librarian provides the
+	 * information for the new book, and the system adds it to the library.
+	 */
+	private void addBook() {
+		// TODO everything
+		
+	}
+
+	/*
+	 * Generate a report with all the books that have been checked out. For each book the report
+	 * shows the date it was checked out and the due date. The system flags the items that are overdue.
+	 * The items are ordered by the book call number. If a subject is provided the report lists only
+	 * books related to that subject, otherwise all the books that are out are listed by the report.
+	 */
+	private void checkoutReport() {
+		// TODO everything
+		
+	}
+
+	/*
+	 * Generate a report with the most popular items in a given year. The librarian provides a year and
+	 * a number n. The system lists out the top n books that where borrowed the most times during that year.
+	 * The books are ordered by the number of times they were borrowed.
+	 */
+	private void popularBooks() {
 		// TODO everything
 		
 	}
