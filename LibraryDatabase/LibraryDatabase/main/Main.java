28cc0b3f774882e0f946d731a8a21f83534c7119
ScottMastro <scott-mastro@hotmail.com>
2014-03-24 19:51:12 -0700

    new borrower

--- LibraryDatabase/LibraryDatabase/main/Main.java
+++ LibraryDatabase/LibraryDatabase/main/Main.java
@@ -5,6 +5,9 @@ import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -71,21 +74,36 @@ public class Main extends JFrame implements ActionListener{
 
 		String[] properties = {"ID", "Password", "Name", "Address", "Phone", "Email", "SIN/S.Num", "Expiry", "Type"};
 		
-		createInputPopup(properties, "Add New Borrower");
-
+		properties = createInputPopup(properties, "Add New Borrower");
+		
+		bid = properties[0];
+		password = properties[1];
+		name = properties[2];
+		address = properties[3];
+		phone = properties[4];
+		emailAddress = properties[5];
+		sinOrStNo = properties[6];
+		expiryDate = properties[7];
+		type = properties[8];
+		
+		//TODO: add borrower with given properties
 	}
 
 
-	private void createInputPopup(String[] labels, String title){
+	@SuppressWarnings("static-access")
+	private String[] createInputPopup(String[] labels, String title){
 		
 		int n = labels.length;
 		Object[] message = new Object[n];
 		JPanel p = new JPanel(new SpringLayout());
+		JTextField[] fields = new JTextField[n];
 		
 		for(int i = 0; i <= n - 1; i++){
 			
 			p.add(new JLabel(labels[i], JLabel.TRAILING));
-			p.add(new JTextField(20));
+			JTextField text = new JTextField(20);
+			fields[i] = text;
+			p.add(text);
 			
 			message[i] = p;
 		}
@@ -96,9 +114,18 @@ public class Main extends JFrame implements ActionListener{
 		                                6, 6,        //initX, initY
 		                                6, 6);       //xPad, yPad
 		
-		JOptionPane.showOptionDialog(null, message, title, JOptionPane.OK_CANCEL_OPTION,
+		final JOptionPane window = new JOptionPane();
+		
+		window.showOptionDialog(null, message, title, JOptionPane.OK_CANCEL_OPTION,
 				JOptionPane.QUESTION_MESSAGE, null, null, null); 
-
+		
+		String[] input = new String[n];
+		
+		for(int i = 0; i <= n - 1; i++){
+			input[i] = fields[i].getText();
+		}
+		
+		return input;
 	}
 
 }
